ebf1609ee6efafa10ac9573f33862ccb60e934b5
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-30 00:35:48 +0200

    Bug Fix

--- apiroutes/maintain/ping/ping-route.js
+++ apiroutes/maintain/ping/ping-route.js
@@ -2,7 +2,7 @@ const nconf = require('nconf');
 const status = require('../../../_assets/json/status/status.json');
 
 module.exports = ({ app }) => {
-    app.get(nconf.get('routes:main:path') + nconf.get('routes:ping:path'), (req, res) => {
+    app.get(`${app.get('mainURL')}${app.get('routes').maintain.ping.path}`, (req, res) => {
         return res.json({
             status: status.STATUS_OK,
         });
